8a5eccb837594f0704b31d8f6faafdf92bcb5299
lima-BEAN <limas9@icloud.com>
2017-07-21 10:05:47 -0500

    First init

---
+++ pythonBasics/hello.py
@@ -0,0 +1,11 @@
+# This program says hellp and ask for my name.
+
+print('Hello World')
+print('What\'s your name?') # ask for their name
+myName = input()    # allow users to input name
+print('It is good to meet you, ' + myName)
+print('The length of you name is:')
+print(len(myName))
+print('What is your age'? ) # Ask for their age
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
